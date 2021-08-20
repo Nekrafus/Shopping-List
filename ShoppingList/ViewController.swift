de1193951e9a265c6f222be6d0f9ef8501fd18bc
Nekrafus <nekrafus@gmail.com>
2021-08-20 17:21:18 +0300

    Initial Commit

---
+++ ShoppingList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShoppingList
+//
+//  Created by Vladimir Pulkhrov on 20.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
